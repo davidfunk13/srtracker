2a8c672343cccb074330673b280d5f93f688142b
Dave Funk <davefunk135@gmail.com>
2018-06-28 14:29:33 -0700

    modal active

---
+++ client/src/components/AddAccountForm/BattleTag/BattleTag.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as modalActionCreators from '../../../actions/modalActions';
+import * as accountsSavedFormActionCreators from '../../../actions/accountActions';
+// import Menu from '../../../Pages/Menu/Menu';
+class BattleTag extends Component {
+    state = {
+        BattleTag: "",
+        Errors: "",
+    };
+
+    handleInputChange = event => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({
+            [name]: value
+        });
+        console.log(`BattleTag input: ${this.state.BattleTag}`)
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        let BattleTag = this.state.BattleTag;
+        let uidOBJ = { uid: this.props.profile.sub, BattleTag: BattleTag}
+        if (BattleTag === "") {
+            console.log('empty')
+            this.setState({ Errors: 'Battletag Cannot be Empty Field!'})
+            return this.state.Errors
+        }
+        if (BattleTag !== "") {
+            this.setState({Errors: []})
+            console.log(uidOBJ)
+            this.props.accountsSavedFormActions.createUserNode(uidOBJ)            
+        }
+    }
+
+
+    render() {
+
+        return (
+            <div className='account-form'>
+                <h1 className='u-margin-bottom-small'>please tell us your battletag.</h1>
+                <p>No account numbers are required, this is simply to put at the top of your spreadsheet in case you want to track another account.</p>
+                <form className="account-form__form">
+                {this.state.Errors ? <div><p>{this.state.Errors}</p></div> : <div><p>no errors to show</p></div> } 
+                    <input
+                        className='account-form__form--BattleTag'
+                        value={this.state.BattleTag}
+                        name="BattleTag"
+                        onChange={this.handleInputChange}
+                        type="text"
+                        placeholder="Battletag"
+                    />
+                    <button className='btn btn--submit-form' onClick={this.handleFormSubmit}>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        showModal: state.showModal,
+        accountsSavedForm: state.accountsSavedForm,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        modalActions: bindActionCreators(modalActionCreators, dispatch),
+        accountsSavedFormActions: bindActionCreators(accountsSavedFormActionCreators, dispatch),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BattleTag)
--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
+import ReactModal from "react-modal";
+import BattleTag from "../../components/AddAccountForm/BattleTag/BattleTag";
 import { connect } from 'react-redux'
 import { bindActionCreators } from "redux";
 import * as modalActionCreators from "../../actions/modalActions";
 import * as accountActionCreators from '../../actions/accountActions'
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "60%",
+    height: "60%",
+    border: "1px solid black"
+  }
+};
+
 class Main extends Component {
+  componentWillMount() {
+    ReactModal.setAppElement("body");
+  }
   render() {
     return (
       <div className="container">
-        <div className='container--logged-out'>
-          <h1 className="heading u-margin-bottom-small">Thank you for logging in! {this.props.profile.name}</h1>
-          
-        </div>
-        <a href="/menu"><button className='btn'>Menu</button></a>
+        <ReactModal isOpen={this.props.showModal} style={customStyles}>
+          <BattleTag {...this.props} />
+          <button
+            className="btn btn--close-modal"
+            onClick={() => this.props.modalActions.closeModal()}
+          >
+            close modal
+          </button>
+        </ReactModal>
+        <h1 className="heading u-margin-bottom-small">Thank you for logging in! {this.props.profile.name}</h1>
+        <button onClick={() => {this.props.modalActions.openModal()}}>Track Account</button>
       </div>
     );
   }
 }
- 
+
 function mapStateToProps(state) {
   return {
     showModal: state.showModal,
