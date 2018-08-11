d0e0e68febc9a9bb7f1bbbfb4c055bfe012fe2a4
Dave Funk <davefunk135@gmail.com>
2018-08-10 21:26:16 -0700

    more prop stripping

--- client/src/components/AddGameForm/AddGameForm.jsx
+++ client/src/components/AddGameForm/AddGameForm.jsx
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
-import MatchMap from './MatchMap/MatchMap';
-import HeroSelected from './HeroSelected/HeroSelected';
-import DidYouWin from './DidYouWin/DidYouWin';
-import PostMatchSR from './PostMatchSR/PostMatchSR';
-import ConfirmGame from './ConfirmGame/ConfirmGame';
+// import MatchMap from './MatchMap/MatchMap';
+// import HeroSelected from './HeroSelected/HeroSelected';
+// import DidYouWin from './DidYouWin/DidYouWin';
+// import PostMatchSR from './PostMatchSR/PostMatchSR';
+// import ConfirmGame from './ConfirmGame/ConfirmGame';
 
 class AddGameForm extends Component {
 
     componentDidMount(){
-        console.log(this.props.addGameForm.gameFormStep)
-        console.log(this.props)
     }
 
     render() {
-        let step = this.props.addGameForm.gameFormStep
-        switch (step) {
-            case 1:
-                step = <MatchMap {...this.props} />
-                break;
-            case 2:
-                step = <HeroSelected {...this.props}/>
-                break;
-            case 3:
-                step = <DidYouWin {...this.props} />
-                break;
-            case 4:
-                step = <PostMatchSR {...this.props} />
-                break;
-            case 5:
-                step = <ConfirmGame {...this.props} />
-                break;
-            default:
-                break;
-        }
-        return (
-            <div>
-                {step}
-            </div>
-        )
+        
+    //     let step = this.props.addGameForm.gameFormStep
+    //     switch (step) {
+    //         case 1:
+    //             step = <MatchMap {...this.props} />
+    //             break;
+    //         case 2:
+    //             step = <HeroSelected {...this.props}/>
+    //             break;
+    //         case 3:
+    //             step = <DidYouWin {...this.props} />
+    //             break;
+    //         case 4:
+    //             step = <PostMatchSR {...this.props} />
+    //             break;
+    //         case 5:
+    //             step = <ConfirmGame {...this.props} />
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     return (
+    //         <div>
+    //             {step}
+    //         </div>
+    //     )
     }
 }
 export default AddGameForm
--- client/src/components/AddSeasonForm/AddSeasonForm.jsx
+++ client/src/components/AddSeasonForm/AddSeasonForm.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
-import StartingSR from './StartingSR/StartingSR';
-import HerosFocused from './HerosFocused/HerosFocused';
-import ConfirmSeason from './ConfirmSeason/ConfirmSeason';
+// import StartingSR from './StartingSR/StartingSR';
+// import HerosFocused from './HerosFocused/HerosFocused';
+// import ConfirmSeason from './ConfirmSeason/ConfirmSeason';
 
 class AddSeasonForm extends Component {
     
     componentDidMount(){
-        console.log(this.props.addSeasonForm.seasonFormStep)
+
     }
 
     render() {
-        let step = this.props.addSeasonForm.seasonFormStep
-        switch (step) {
-            case 1:
-                step = <StartingSR {...this.props} />
-                break;
-            case 2:
-                step = <HerosFocused {...this.props}/>
-                break;
-            case 3:
-                step = <ConfirmSeason {...this.props} />
-                break;
-            default:
-                break;
-        }
-        return (
-            <div>
-                {step}
-            </div>
-        )
+    //     let step = this.props.addSeasonForm.seasonFormStep
+    //     switch (step) {
+    //         case 1:
+    //             step = <StartingSR {...this.props} />
+    //             break;
+    //         case 2:
+    //             step = <HerosFocused {...this.props}/>
+    //             break;
+    //         case 3:
+    //             step = <ConfirmSeason {...this.props} />
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     return (
+    //         <div>
+    //             {step}
+    //         </div>
+    //     )
     }
 }
 export default AddSeasonForm
