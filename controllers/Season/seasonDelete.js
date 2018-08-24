5047ce17771cc1fd5d536b44d2bba8dc001cced5
Dave Funk <davefunk135@gmail.com>
2018-08-24 15:52:54 -0700

    i believe it is deleting seasons with their games and pulling the appropriate object ids as well. testing underway

--- controllers/Season/seasonDelete.js
+++ controllers/Season/seasonDelete.js
@@ -16,15 +16,39 @@ const seasonDelete = {
     })
   },
   deleteOneSeasonById: (req, res) => {
-    db.Battletag.findByIdAndUpdate(req.query.battletag, {$pullAll: { Seasons: [req.query.seasonId] } }, {new:true}).then(result=>{
+    let gameIds = []
+    db.Game.find({ SeasonId: req.query.seasonId }).then(seasonGames => {
+      console.log(seasonGames)
+      seasonGames.forEach(game => {
+        gameIds.push(game._id)
+      })
+      console.log('gameIds')
+      console.log(gameIds)
+      db.Season.findByIdAndUpdate(req.query.seasonId, { $pullAll: { Games: gameIds } }, { new: true }).then(result => {
+        console.log(result)
+        db.Game.deleteMany({ SeasonId: req.query.seasonId }).then(response => {
+          console.log(response)
+        })
+       db.Battletag.findByIdAndUpdate(req.query.battletag, {$pullAll: { Seasons: [req.query.seasonId] } }, {new:true}).then(result=>{
       console.log(result)
       res.json({
         message: 'Removed Season',
         payload: result
       })
     })
+      })
+    })
+
+
+    // db.Battletag.findByIdAndUpdate(req.query.battletag, {$pullAll: { Seasons: [req.query.seasonId] } }, {new:true}).then(result=>{
+    //   console.log(result)
+    //   res.json({
+    //     message: 'Removed Season',
+    //     payload: result
+    //   })
+    // })
     // db.Battletag.findById(req.query.battletag).then(shit=>{
-      // console.log(shit)
+    // console.log(shit)
     // })
   },
   deleteSeasonsByBattletagId: (req, res) => {
